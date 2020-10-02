56e668919872cfc646748585912577165bc3c672
jemeson007 <emesonjonah@gmail.com>
2020-10-02 15:08:43 +0100

    Update EmployeeSelection.java

--- EmployeeSelection.java
+++ EmployeeSelection.java
@@ -7,36 +7,39 @@ class EmployeeSelection{
   
   Scanner sc = new Scanner(System.in);
   
-  public HashMap<String, Integer> theEmployees = new HashMap<String, Integer>();
+  HashMap<String, Double> theEmployees = new HashMap<String, Double>();
 
 
-  public HashMap<String, Integer> theOtherEntities = new HashMap<String, Integer>();
-  theOtherEntities.put("Reviewer", 1);
-  theOtherEntities.put("Owner", 2);
+  HashMap<String, Double> theOtherEntities = new HashMap<String, Double>();
+  theOtherEntities.put("Reviewer", (double) 1);
+  theOtherEntities.put("Owner", (double) 2);
 
 
  for(int i = 1; i <=5; i++){
    System.out.println("Enter Employee name: ");
-   String thePresentEmployee = sc.getNext();
-   theEmployees.put("thePresentEmployee", i);
+   String thePresentEmployee = sc.next();
+   theEmployees.put("thePresentEmployee",(double) i);
   
  }
  
   System.out.println("Enter stipulated Amount: ");
-  public double stipulatedAmount = sc.nextDouble();
+  double stipulatedAmount = sc.nextDouble();
   
   System.out.println("Enter chosen Employee name: ");
-  public String chosenEmployee = sc.nextString();
-
-  theEmployees.get(chosenEmployee) = 0.4 * stipulatedAmount;
-
-  theOtherEntities.get("Reviewer") = 0.1 * stipulatedAmount;
+  String chosenEmployee = sc.next();
   
-  theOtherEntities.get("Owner") = 0.5 * stipulatedAmount;
+  double employeeShare = 0.4 * stipulatedAmount;
+  theEmployees.put(chosenEmployee, (double) employeeShare);
 
-  System.out.println("Value for " + chosenEmployee + " is " + theEmployee.get(chosenEmployee);
-  System.out.println("Value for the Reviewer is " + theOtherEntities.get("Reviewer");
-  System.out.println("Value for the Owner is " + theOtherEntities.get("Owner");
+  double reviewerShare = 0.1 * stipulatedAmount;
+  theOtherEntities.put("Reviewer",  reviewerShare); 
+  
+  double ownerShare = 0.5 * stipulatedAmount;
+  theOtherEntities.put("Owner", (double) ownerShare);
+
+  System.out.println("Value for " + chosenEmployee + " is " + theEmployees.get(chosenEmployee));
+  System.out.println("Value for the Reviewer is " + theOtherEntities.get("Reviewer"));
+  System.out.println("Value for the Owner is " + theOtherEntities.get("Owner"));
 
  
     
